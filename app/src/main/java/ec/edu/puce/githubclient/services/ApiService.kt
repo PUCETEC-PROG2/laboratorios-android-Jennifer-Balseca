0b98f4be69e8f6635cad7ef8923365dda4f73f0a
JenniferBalseca <jibalseca@puce.edu.ec>
2026-05-21 14:38:13 -0500

    filtros de repositorios

--- app/src/main/java/ec/edu/puce/githubclient/services/ApiService.kt
+++ app/src/main/java/ec/edu/puce/githubclient/services/ApiService.kt
@@ -1,9 +1,17 @@
 package ec.edu.puce.githubclient.services
 import ec.edu.puce.githubclient.models.Repository
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("/user/repos")
-    suspend fun getRepositories () : List<Repository>
+    suspend fun getRepositories (
+        @Query("sort") created: String = "created",
+        @Query("direction") direction: String = "desc",
+        @Query("affiliation") affiliation: String = "owner",
+        @Query("per_oage") perPage: Int = 100,
+        @Query("t") t: String = "${System.currentTimeMillis()}",
+
+    ) : List<Repository>
 }
 
